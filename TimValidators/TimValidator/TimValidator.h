82168ea0c08705bde725ec968c12cfba7812335f
Thommzy <tomiwaobeisun@yahoo.com>
2022-01-05 07:54:30 +0100

    add framework

---
+++ TimValidators/TimValidator/TimValidator.h
@@ -0,0 +1,18 @@
+//
+//  TimValidator.h
+//  TimValidator
+//
+//  Created by Tim on 05/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TimValidator.
+FOUNDATION_EXPORT double TimValidatorVersionNumber;
+
+//! Project version string for TimValidator.
+FOUNDATION_EXPORT const unsigned char TimValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimValidator/PublicHeader.h>
+
+
---
+++ TimValidators/TimValidator/Validator.swift
@@ -0,0 +1,20 @@
+//
+//  Validator.swift
+//  TimValidator
+//
+//  Created by Tim on 05/01/2022.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func validEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    public static func sayHello() {
+        print("Hello pretty lady. How are you doing????")
+    }
+}
